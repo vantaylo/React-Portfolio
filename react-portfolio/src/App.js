81b812af546b37fc81e9516504574b10a9b43f6e
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-29 15:42:01 -0500

    create pages and routes for pages. Imported to app.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -6,6 +6,9 @@ import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 class App extends React.Component {
   constructor(props) {
@@ -53,6 +56,27 @@ class App extends React.Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            render={() => <AboutPage title={this.state.about.title} />}
+          />
+          <Route
+            path="/contact"
+            render={() => <ContactPage title={this.state.contact.title} />}
+          />
+
           <Footer />
         </Container>
       </Router>
---
+++ react-portfolio/src/pages/ContactPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ContactPage(props) {
+  return <div></div>;
+}
+
+export default ContactPage;
