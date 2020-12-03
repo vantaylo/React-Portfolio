00a4437d2d7b44ee6793fa22b4953fdb25be9c7a
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-12-02 22:40:09 -0600

    removed footer line

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <footer className="mt-5">
       <Container fluid={true}>
-        <Row className="border-top justify-content-between p-3">
+        <Row className=" justify-content-between p-3">
           <Col className="p-0" md={4} sm={12}>
             <a
               href="mailto:vanessalyn.taylor@gmail.com"
