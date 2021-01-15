904ac9ea46abddd8c077a34b0431a11521caef26
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-01-14 19:29:49 -0600

    added hyperlinked icon for resume with styling

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -1,4 +1,5 @@
 import React from "react";
+import resumeIcon from "../assets/images/resume-icon.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLinkedin,
@@ -9,18 +10,22 @@ import {
 export default function SocialFollow() {
   return (
     <div className="row">
+      <a href="https://www.linkedin.com/in/vanessalyn-taylor/"className="linkedin social">
+      <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+      
+      <a href="https://www.dropbox.com/s/edrd4t6knct2k5p/VanessaTaylor_Resume.pdf?dl=0" >
+        <img className="resume-icon" src={resumeIcon} alt="Resume" />
+      </a>
+
       <a href="https://github.com/vantaylo" className="github social">
         <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
-      <a
-        href="https://www.linkedin.com/in/vanessalyn-taylor/"
-        className="linkedin social"
-      >
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-      </a>
-      <a href="https://twitter.com/VanessaL_Taylor" className="twitter social">
+
+      {/* <a href="https://twitter.com/VanessaL_Taylor" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
-      </a>
+      </a> */}
+
     </div>
   );
 }
