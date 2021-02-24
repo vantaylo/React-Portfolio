62e0f831d11a4481ca6115a1bd4ee8ec14ce62c8
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-02-24 13:57:33 -0600

    updated Trailme app link

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -29,7 +29,7 @@ class Carousel extends React.Component {
           title: "TRAILme App",
           subTitle: "A Full-Stack application using Node, Express, React & MongoDB to help find hiking trails anywhere!",
           imgSrc: project1,
-          link: "https://vt-react-portfolio.herokuapp.com/",
+          link: "https://vt-trailme.herokuapp.com/",
           selected: false,
         },
         {
