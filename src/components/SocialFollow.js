cd7913d13607c79eaad21a09b8dc64682b893c76
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2021-03-09 11:19:51 -0600

    updated resume link

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -15,7 +15,7 @@ export default function SocialFollow() {
         <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
 
-      <a href="https://www.dropbox.com/s/edrd4t6knct2k5p/VanessaTaylor_Resume.pdf?dl=0" >
+      <a href="https://www.dropbox.com/s/5wojsr0162io4te/VanessaTaylor_Resume.pdf?dl=0" >
         <img className="resume-icon" src={resumeIcon} alt="Resume" />
       </a>
 
